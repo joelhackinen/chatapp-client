e04f0f6a9d269eaa3d0ac556597b4b0f5ca1afa0
Joel <joel.hakkinen@gmail.com>
2022-11-03 23:31:35 +0200

    added rooms

--- src/App.js
+++ src/App.js
@@ -3,22 +3,50 @@ import { useState } from 'react'
 const socket = io.connect('http://localhost:3001')
 
 const App = () => {
+  const [room, setRoom] = useState('')
+  const [roomField, setRoomField] = useState('')
   const [message, setMessage] = useState('')
   const [messages, setMessages] = useState([])
 
   const sendMessage = () => {
-    socket.emit('send_message', { msg: message })
+    socket.emit('send-message', message, room)
     setMessage('')
   }
 
-  socket.on('receive_message', (data) => {
-    setMessages(messages.concat(data.msg))
+  const joinRoom = () => {
+    setRoom(roomField)
+    socket.emit('join-room', roomField)
+    setRoomField('')
+  }
+
+  socket.on('receive-message', (data) => {
+    setMessages(messages.concat(data))
   })
 
 
   return (
     <div>
-      <input value={message} onChange={(event) => setMessage(event.target.value)}/>
+      <h2>
+        {socket.id}
+      </h2>
+      <h3>
+        message will be sent to {room ? `room ${room}` : 'everyone'}
+      </h3>
+      <div>
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          placeholder='message'
+        />
+      </div>
+      <div>
+        <input
+          value={roomField}
+          onChange={(event) => setRoomField(event.target.value)}
+          placeholder='room'
+        />
+        <button onClick={joinRoom}>Join</button>
+      </div>
       <button onClick={sendMessage}>Send</button>
       <h3>Received messages</h3>
       <ul>
